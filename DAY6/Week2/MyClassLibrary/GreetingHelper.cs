7d0ab5f8ba17be23c3678ce961a97af42666b38e
rajivkumar22 <rajivkumar22@example.com>
2026-01-12 15:49:13 +0530

    Added DAY6 projects

---
+++ DAY6/Week2/MyClassLibrary/GreetingHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace MyClassLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+           if (string.IsNullOrEmpty(name))
+            {
+                return "Hello,Guest!";
+            }
+           else
+            {
+                return  $"Hello,{name}";
+            }
+        }
+    }
+}
---
+++ DAY6/Week2/libraryproject/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyClassLibrary;
+
+namespace libraryproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int result= GreetingHelper.GetDouble(5);
+            Console.WriteLine("Enter the name:");
+            string name = Console.ReadLine()??"";
+            string result = GreetingHelper.GetGreeting(name);
+            Console.WriteLine(result);
+           // Console.WriteLine("Hello, World!");
+        }
+    }
+}
