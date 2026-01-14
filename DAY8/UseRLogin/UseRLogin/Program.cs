6bfbfe6d49692b63955fc8c04966399304576074
rajivkumar22 <rajivkumar22@example.com>
2026-01-14 15:18:55 +0530

    Added User Login message processor console app

---
+++ DAY8/UseRLogin/UseRLogin/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace UseRLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter UserName and Message: ");
+            string input = Console.ReadLine();
+            string[] parts = input.Split('|');
+            string userName = parts[0];
+            string rawMessage = parts[1];
+            string cleanedMessage = rawMessage.Trim().ToLower();
+            string standardmessage = "login successful";
+            string status;
+            if (!cleanedMessage.Contains("successful"))
+            {
+                status = "LOGIN FAILED";
+            }
+            else if (cleanedMessage.Equals(standardmessage))
+            {
+                status = "LOGIN SUCCESS";
+            }
+            else
+            {
+                status = "LOGIN SUCCESS (CUSTOM MESSAGE)";
+            }
+            Console.WriteLine($"User     :  {userName}");
+            Console.WriteLine($"Message  :{ cleanedMessage}");
+            Console.WriteLine($"Status   :{ status}");
+        }
+    }
+}
