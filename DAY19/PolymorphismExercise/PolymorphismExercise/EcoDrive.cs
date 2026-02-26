e2c20a1851cf42e99b09446156945d776d4e1dd2
rajivkumar22 <rajivkumar22@example.com>
2026-02-26 22:42:10 +0530

    Polymorphism Exercise Uploaded

---
+++ DAY19/PolymorphismExercise/PolymorphismExercise/EcoDrive.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismExercise
+{
+    abstract class Vehicle
+    {
+        public string ModelName { get; set; }
+        public Vehicle(string modelname)
+        {
+            ModelName = modelname;
+        }
+        public abstract void Move();
+        public virtual string GetFuelStatus()
+        {
+            return "Fuel level is stable";
+        }
+    }
+    class ElectricCar : Vehicle
+    {
+
+        public ElectricCar(string modelname) : base(modelname) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is gliding silently on battery power");
+        }
+        public override string GetFuelStatus()
+        {
+            return $"{ModelName} battery is at 80 %.";
+
+        }
+
+
+    }
+
+    class HeavyTruck : Vehicle
+    {
+        public HeavyTruck(string modelname) : base(modelname) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is hauling cargo with high-torque diesel power.");
+        }
+
+    }
+
+    class CargoPlane : Vehicle
+    {
+        public CargoPlane(string modelname) : base(modelname) { }
+        public override void Move()
+        {
+            Console.WriteLine($"{ModelName} is ascending to 30,000 feet.");
+        }
+
+
+
+        public override string GetFuelStatus()
+        {
+            return $"{base.GetFuelStatus()} : Checking jet fuel reserves...";
+
+        }
+    }
+    internal class EcoDrive
+    {
+        static void Main(string[] args)
+        {
+            Vehicle[] vehicle = new Vehicle[3]
+       {
+             new ElectricCar("Volvo"),
+            new  HeavyTruck("Tmax"),
+            new CargoPlane("Wkot")
+         };
+            foreach (var ch in vehicle)
+            {
+                ch.Move();
+                Console.WriteLine(ch.GetFuelStatus());
+            }
+
+
+        }
+    }
+}
---
+++ DAY19/PolymorphismExercise/PolymorphismExercise/UtilityBillingSystem .cs
@@ -0,0 +1,126 @@
+﻿
+
+using PolymorphismExercise;
+
+namespace PolymorphismExercise
+{
+    abstract class UtilityBill
+    {
+        public int ConsumerId { get; set; }
+        public string? ConsumerName { get; set; }
+        public decimal UnitsConsumed { get; set; }
+
+        public decimal RatePerUnit { get; set; }
+        public abstract decimal CalculateBillAmount();
+        public UtilityBill(int id, string name, decimal units, decimal rate)
+        {
+            ConsumerId = id;
+            ConsumerName = name;
+            UnitsConsumed = units;
+            RatePerUnit = rate;
+        }
+        public virtual decimal CalculateTax(decimal billAmount)
+        {
+            return 0.05m * billAmount;
+
+        }
+
+        public void PrintBill()
+        {
+            decimal billAmount = CalculateBillAmount();
+            decimal tax = CalculateTax(billAmount);
+            decimal finalAmount = billAmount + tax;
+
+            Console.WriteLine("==================================");
+            Console.WriteLine($"Consumer ID :   {ConsumerId}");
+            Console.WriteLine($"Consumer Name : {ConsumerName}");
+            Console.WriteLine($"Units Consumed: {UnitsConsumed}");
+            Console.WriteLine($"Base Amount   : {billAmount}");
+            Console.WriteLine($"Tax Amount    : {tax}");
+            Console.WriteLine($"Final Amount  : {finalAmount}");
+            Console.WriteLine("==================================\n");
+        }
+
+    }
+
+    class ElectricityBill : UtilityBill
+    {
+        public ElectricityBill(int id, string name, decimal units, decimal rate)
+            : base(id, name, units, rate) { }
+
+        public override decimal CalculateBillAmount()
+        {
+            decimal amount = UnitsConsumed * RatePerUnit;
+
+            if (UnitsConsumed > 300)
+            {
+                amount += amount * 0.10m;
+            }
+
+            return amount;
+        }
+
+
+    }
+
+
+    class WaterBill : UtilityBill
+    {
+        public WaterBill(int id, string name, decimal units, decimal rate)
+            : base(id, name, units, rate) { }
+
+        public override decimal CalculateBillAmount()
+        {
+            return UnitsConsumed * RatePerUnit;
+        }
+
+        public override decimal CalculateTax(decimal billAmount)
+        {
+            return billAmount * 0.02m;
+        }
+    }
+
+
+    class GasBill : UtilityBill
+    {
+        public GasBill(int id, string name, decimal units, decimal rate)
+            : base(id, name, units, rate) { }
+
+        public override decimal CalculateBillAmount()
+        {
+            decimal amount = UnitsConsumed * RatePerUnit;
+            return amount + 150m;
+        }
+
+        public override decimal CalculateTax(decimal billAmount)
+        {
+            return 0m;
+        }
+    }
+
+
+
+
+
+    internal class UtilityBillingSystem
+    {
+
+
+        static void Main(string[] args)
+        {
+            List<UtilityBill> bills = new List<UtilityBill>
+            {
+                new ElectricityBill(1, "Rahul", 350, 6),
+                new WaterBill(2, "Sneha", 200, 3),
+                new GasBill(3, "Amit", 100, 5)
+            };
+
+            foreach (var bill in bills)
+            {
+                bill.PrintBill();
+            }
+
+        }
+    }
+}
+
