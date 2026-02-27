4cd49928474e52700c40a0fd535b8ae248f9b083
rajivkumar22 <rajivkumar22@example.com>
2026-02-27 19:30:20 +0530

    added DAY22

---
+++ DAY22/OLADriver/OlaDriver/OlaDrivercs.cs
@@ -0,0 +1,71 @@
+﻿
+
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace SatPractice
+{
+    class OlaDriver {
+        public int DriverId{ get; set; }
+        public string Name { get; set; }
+        public string VehicleNumber { get; set; }
+        public int Rides { get; set; }
+        public List<Ride> rides = new List<Ride>();
+        public void AddRides(int rideid, String from, string to, decimal fare)
+        {
+            Ride ride = new Ride(rideid, from, to, fare);
+            rides.Add(ride);
+
+        }
+      
+
+    }
+    class Ride
+        {
+        public int RideId { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public decimal Fare { get; set; }
+
+
+        public  Ride(int rideid,String from,string to,decimal fare)
+        {
+            RideId = rideid;
+            From = from;
+            To = to;
+            Fare = fare;
+        }
+        public override string ToString()
+        {
+            return $"RideId:{RideId} from {From} To {To} Fare {Fare} ";
+        }
+
+    }
+
+    internal class OlaDrivercs
+    {
+        static void Main(string[] args)
+        {
+            OlaDriver driver1 = new OlaDriver()
+            {
+                DriverId = 101,
+                Name = "pramod",
+                VehicleNumber = "A1B",
+                Rides = 2
+
+            };
+            driver1.AddRides(101, "mohali", "kharar", 650);
+            List<OlaDriver> drivers = new List<OlaDriver>();
+            drivers.Add(driver1);
+           // driver.Add(driver1);
+
+            foreach(var driver in drivers)
+            {
+                Console.WriteLine($"Driver name {driver.Name} ");
+            }
+           
+            
+
+        }
+    }
+}
---
+++ DAY22/OLADriver/OlaDriver/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SatPractice
+{
+    internal class Program
+    {
+
+        static bool Pancheck(string s)
+        {
+            if (s.Length != 10)
+                return false;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (!char.IsUpper(s[i]))
+                    return false;
+            }
+
+            for (int i = 5; i < 9; i++)
+            {
+                if (!char.IsDigit(s[i]))
+                    return false;
+            }
+
+            if (!char.IsUpper(s[9]))
+                return false;
+
+            return true;
+        }
+
+        public static void Main()
+        {
+            string pan = "APFP1209F";
+            Console.WriteLine(Pancheck(pan));
+           
+            string name = "Rajiv";
+            bool validfirstname = Regex.IsMatch(name, @"^[A-Z]{1}[a-z]{2,5}$");
+            Console.WriteLine(validfirstname);
+
+        }
+    }
+        
+    
+}
