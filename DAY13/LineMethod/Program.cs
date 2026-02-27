31c010da55a39eb6b6c4391dbd4b638b46b0d047
rajivkumar22 <rajivkumar22@example.com>
2026-02-27 18:08:27 +0530

    added

---
+++ DAY13/LineMethod/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GymCharges
+{
+    internal class LineMethod
+    {
+        static void Main(string[] args)
+        {
+            DisplayLine();
+            DisplayLine('+');
+            DisplayLine('$', 60);
+        }
+
+        static void DisplayLine()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write('-');
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplayLine(char c)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplayLine(char c, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+}
