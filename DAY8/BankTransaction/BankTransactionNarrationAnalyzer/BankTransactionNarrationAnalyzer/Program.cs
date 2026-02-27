61ecae6677257406c0e73745bf6c01d5553447e8
rajivkumar22 <rajivkumar22@example.com>
2026-02-27 14:56:01 +0530

    banktransaction project

---
+++ DAY8/BankTransaction/BankTransactionNarrationAnalyzer/BankTransactionNarrationAnalyzer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BankTransactionNarrationAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] parts = input.Split('#');
+
+            string transactionId = parts[0];
+            string accountHolder = parts[1];
+            string narration = parts[2];
+
+            narration = narration.Trim();
+
+            while (narration.Contains("  "))
+            {
+                narration = narration.Replace("  ", " ");
+            }
+
+            narration = narration.ToLower();
+
+            bool hasKeyword = false;
+
+            if (narration.Contains("deposit") ||
+                narration.Contains("withdrawal") ||
+                narration.Contains("transfer"))
+            {
+                hasKeyword = true;
+            }
+
+            string standardNarration = "cash deposit successful";
+
+            bool isExactMatch = narration.Equals(standardNarration);
+
+            string category;
+
+            if (!hasKeyword)
+            {
+                category = "NON-FINANCIAL TRANSACTION";
+            }
+            else if (hasKeyword && isExactMatch)
+            {
+                category = "STANDARD TRANSACTION";
+            }
+            else
+            {
+                category = "CUSTOM TRANSACTION";
+            }
+
+            Console.WriteLine("Transaction ID : " + transactionId);
+            Console.WriteLine("Account Holder : " + accountHolder);
+            Console.WriteLine("Narration      : " + narration);
+            Console.WriteLine("Category       : " + category);
+        }
+    }
+}
