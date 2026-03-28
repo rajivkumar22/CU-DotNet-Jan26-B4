3dda5e1179fa45f518cec107a030a78889b29b50
rajivkumar22 <rajivkumar22@example.com>
2026-03-28 12:50:47 +0530

    added week12

---
+++ Assessments/WEEK12/LogTrack/IdentityService/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using IdentityService.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public AuthController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginModel dto)
+    {
+        if (dto.Email != "admin@test.com" || dto.Password != "1234")
+            return Unauthorized();
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, "1"),
+            new Claim(JwtRegisteredClaimNames.Email, dto.Email),
+            new Claim(ClaimTypes.Role, "Manager") 
+        };
+
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(_config["Jwt:Key"])
+        );
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: creds
+        );
+
+        return Ok(new
+        {
+            access_token = new JwtSecurityTokenHandler().WriteToken(token)
+        });
+    }
+}
---
+++ Assessments/WEEK12/LogTrack/TrackingService/Controllers/TrackingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TrackingController : ControllerBase
+{
+    [HttpGet("gps")]
+    [Authorize(Roles = "Manager")]
+    public IActionResult GetGps()
+    {
+        var userEmail = User.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+        var userId = User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value;
+        var role = User.FindFirst(System.Security.Claims.ClaimTypes.Role)?.Value;
+
+        return Ok(new
+        {
+            success = true,
+            message = "GPS data fetched successfully",
+            user = new
+            {
+                id = userId,
+                email = userEmail,
+                role = role
+            },
+            data = new
+            {
+                latitude = "24.999",
+                longitude = "89.9990",
+                vehicle = "VOLVO7777"
+            }
+        });
+    }
+}
